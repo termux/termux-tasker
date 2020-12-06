b042303752a1a3491b7f7b186c51b978608418cb
agnostic-apollo <agnosticapollo@gmail.com>
2020-12-06 19:07:39 +0500

    Switched to AndroidX and fixed test code.

--- app/src/androidTest/java/com/termux/tasker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/termux/tasker/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package com.termux.tasker;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,13 +14,13 @@ import static org.junit.Assert.assertEquals;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
 
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.termux.tasker", appContext.getPackageName());
     }

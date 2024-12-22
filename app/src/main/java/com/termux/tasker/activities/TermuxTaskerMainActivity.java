3c601783b39965353d47ea6bd654ee256314ade5
agnostic-apollo <agnosticapollo@gmail.com>
2024-12-23 04:04:57 +0500

    Changed: Rename `TermuxTaskerActivity` to `TermuxTaskerMainActivity` stage 2
    The `TermuxTaskerLauncherActivity` activity alias has been added for `TermuxTaskerMainActivity`. This will allow disabling the launcher activity, but still allow the main activity to be launched from Termux app. The launcher activity will also now be allowed to be enabled again without having to reinstall the app with the `Enable Launcher Icon` button in the main activity if its currently disabled.

--- app/src/main/java/com/termux/tasker/activities/TermuxTaskerMainActivity.java
+++ app/src/main/java/com/termux/tasker/activities/TermuxTaskerMainActivity.java
@@ -9,19 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.termux.shared.activity.media.AppCompatActivityUtils;
 import com.termux.shared.android.PackageUtils;
 import com.termux.shared.logger.Logger;
+import com.termux.shared.markdown.MarkdownUtils;
 import com.termux.shared.termux.TermuxConstants;
 import com.termux.shared.termux.theme.TermuxThemeUtils;
 import com.termux.shared.theme.NightMode;
 import com.termux.tasker.R;
 
-public class TermuxTaskerActivity extends AppCompatActivity {
+public class TermuxTaskerMainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = "TermuxTaskerActivity";
+    public static final String LOG_TAG = "TermuxTaskerMainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_termux_tasker);
+        setContentView(R.layout.activity_termux_tasker_main);
 
         // Set NightMode.APP_NIGHT_MODE
         TermuxThemeUtils.setAppNightMode(this);
@@ -33,14 +34,62 @@ public class TermuxTaskerActivity extends AppCompatActivity {
         TextView pluginInfo = findViewById(R.id.textview_plugin_info);
         pluginInfo.setText(getString(R.string.plugin_info, TermuxConstants.TERMUX_GITHUB_REPO_URL,
                 TermuxConstants.TERMUX_TASKER_GITHUB_REPO_URL));
+    }
 
-        Button disableLauncherIconButton = findViewById(R.id.btn_disable_launcher_icon);
-        disableLauncherIconButton.setOnClickListener(v -> {
-            String message = getString(com.termux.shared.R.string.msg_disabling_launcher_icon, TermuxConstants.TERMUX_TASKER_APP_NAME);
-            Logger.logInfo(LOG_TAG, message);
-            PackageUtils.setComponentState(TermuxTaskerActivity.this,
-                    TermuxConstants.TERMUX_TASKER_PACKAGE_NAME, TermuxConstants.TERMUX_TASKER.TERMUX_TASKER_ACTIVITY_NAME,
-                    false, message, true);
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Logger.logVerbose(LOG_TAG, "onResume");
+
+        setChangeLauncherActivityStateViews();
+    }
+
+
+
+    private void setChangeLauncherActivityStateViews() {
+        String packageName = TermuxConstants.TERMUX_TASKER_PACKAGE_NAME;
+        String className = TermuxConstants.TERMUX_TASKER_APP.TERMUX_TASKER_LAUNCHER_ACTIVITY_NAME;
+
+        TextView changeLauncherActivityStateTextView = findViewById(R.id.textview_change_launcher_activity_state_details);
+        changeLauncherActivityStateTextView.setText(MarkdownUtils.getSpannedMarkdownText(this,
+                getString(R.string.msg_change_launcher_activity_state_info, packageName, getClass().getName())));
+
+        Button changeLauncherActivityStateButton = findViewById(R.id.button_change_launcher_activity_state);
+        String stateChangeMessage;
+        boolean newState;
+
+        Boolean currentlyDisabled = PackageUtils.isComponentDisabled(this,
+                packageName, className, false);
+        if (currentlyDisabled == null) {
+            Logger.logError(LOG_TAG, "Failed to check if \"" + packageName + "/" + className + "\" launcher activity is disabled");
+            changeLauncherActivityStateButton.setEnabled(false);
+            changeLauncherActivityStateButton.setAlpha(.5f);
+            changeLauncherActivityStateButton.setText(com.termux.shared.R.string.action_disable_launcher_icon);
+            changeLauncherActivityStateButton.setOnClickListener(null);
+            return;
+        }
+
+        changeLauncherActivityStateButton.setEnabled(true);
+        changeLauncherActivityStateButton.setAlpha(1f);
+        if (currentlyDisabled) {
+            changeLauncherActivityStateButton.setText(com.termux.shared.R.string.action_enable_launcher_icon);
+            stateChangeMessage = getString(com.termux.shared.R.string.msg_enabling_launcher_icon, TermuxConstants.TERMUX_TASKER_APP_NAME);
+            newState = true;
+        } else {
+            changeLauncherActivityStateButton.setText(com.termux.shared.R.string.action_disable_launcher_icon);
+            stateChangeMessage = getString(com.termux.shared.R.string.msg_disabling_launcher_icon, TermuxConstants.TERMUX_TASKER_APP_NAME);
+            newState = false;
+        }
+
+        changeLauncherActivityStateButton.setOnClickListener(v -> {
+            Logger.logInfo(LOG_TAG, stateChangeMessage);
+            String errmsg = PackageUtils.setComponentState(this,
+                    packageName, className, newState, stateChangeMessage, true);
+            if (errmsg == null)
+                setChangeLauncherActivityStateViews();
+            else
+                Logger.logError(LOG_TAG, errmsg);
         });
     }
 
